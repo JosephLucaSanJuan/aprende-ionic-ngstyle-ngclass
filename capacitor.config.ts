662b944476ea0e693e8996fde7578d91a00a67b8
JosephLucaSanJuan <jurassicjoe03@gmail.com>
2024-01-11 19:55:43 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'aprende-ionic-ngstyle-ngclass',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
